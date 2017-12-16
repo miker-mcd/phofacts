915b82c9b4aabf1502c5f3801e30f5a9c3c0a70d
miker-mcd <michaelrmcd@gmail.com>
2017-12-15 23:09:53 -0800

    Add bg image and collect_photo methods

--- hello.py
+++ hello.py
@@ -19,6 +19,18 @@ maps_url = "https://www.google.com/maps/embed/v1/search?key="
 def home():
   return render_template('index.html')
 
+def collect_photos(restaurants):
+  photos = []
+  for restaurant in restaurants:
+    photos.append(restaurant['photos'])
+  return photos;
+
+def collect_photo_refs(photos):
+  photo_refs = []
+  for photo in photos:
+    photo_refs.append(photo[0]['photo_reference'])
+  return photo_refs;
+
 @app.route('/restaurants', methods=['POST'])
 def search_restaurants():
   zipcode = request.form['query']
@@ -29,5 +41,10 @@ def search_restaurants():
   map_req = maps_url + maps_consumer_key + Markup('&zoom=10&q=pho+restaurants+in+') + zipcode
 
   restaurants = search_json["results"]
+  # print(restaurants[0]['photos'][0]['photo_reference'])
+
+  photo_list = collect_photos(restaurants);
+  print(collect_photo_refs(photo_list))
 
   return render_template('show_restaurants.html', map_req=map_req, restaurants=restaurants)
+
